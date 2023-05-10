28f052ebfc4208df43627433e66721b0b5990f37
Moti-Sultan <motisultannur@gmail.com>
2023-05-10 17:07:57 +0200

    password hasher on event POSTMAN

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,11 +3,13 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use ApiPlatform\Metadata\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ApiResource]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
